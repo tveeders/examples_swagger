708eb817d72378dffbf9ebe82f90fe7f0cc57843
Timo Veeders <timo.veeders@metronom.com>
2023-02-28 08:42:54 +0100

    Serve non-static openapi document

--- src/main/java/de/metro/example/swagger/HelloSwaggerApplication.java
+++ src/main/java/de/metro/example/swagger/HelloSwaggerApplication.java
@@ -8,6 +8,11 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+import io.swagger.v3.jaxrs2.integration.JaxrsAnnotationScanner;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import io.swagger.v3.oas.integration.SwaggerConfiguration;
+
+import java.util.Collections;
 
 public class HelloSwaggerApplication extends Application<HelloSwaggerConfiguration> {
 
@@ -30,11 +35,25 @@ public class HelloSwaggerApplication extends Application<HelloSwaggerConfigurati
         });
     }
 
+    private void setupSwagger(
+            final HelloSwaggerConfiguration configuration,
+            final Environment environment ) {
+
+        final SwaggerConfiguration swaggerConfiguration = new SwaggerConfiguration()
+                .openAPI( configuration.openAPI )
+                .prettyPrint( true )
+                .scannerClass( JaxrsAnnotationScanner.class.getName() )
+                .resourceClasses( Collections.singleton( PetResource.class.getName() ) );
+
+        environment.jersey().register( new OpenApiResource().openApiConfiguration( swaggerConfiguration ) );
+    }
+
     @Override
     public void run(final HelloSwaggerConfiguration configuration,
                     final Environment environment) {
         environment.jersey().register(new PetResource( new Shelter() ) );
         environment.jersey().register(new Swagger());
+        setupSwagger( configuration, environment );
     }
 
 }
--- src/main/java/de/metro/example/swagger/HelloSwaggerConfiguration.java
+++ src/main/java/de/metro/example/swagger/HelloSwaggerConfiguration.java
@@ -3,9 +3,11 @@ package de.metro.example.swagger;
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.hibernate.validator.constraints.*;
 import javax.validation.constraints.*;
 
 public class HelloSwaggerConfiguration extends Configuration {
     public SwaggerBundleConfiguration swagger;
+    public OpenAPI openAPI;
 }
